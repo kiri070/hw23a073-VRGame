5c0c70727b6c55c131d1fe77adc8020fef04ff57
kiri_main <hw23a073@oecu.jp>
2026-04-16 23:01:58 +0900

    スキル1(仮実装)

--- Assets/My/Prefabs/Sword/Red/Sword_red.cs
+++ Assets/My/Prefabs/Sword/Red/Sword_red.cs
@@ -5,16 +5,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Sword_red : MonoBehaviour
 {
+    //===== エフェクト =====
+    public GameObject hitEffect;
+    //エンチャントレベル1
     public GameObject explosionEffect;
-    public GameObject multiSlashEffect;
+    public GameObject fireEffect;
+    //エンチャントレベル2
+
+    //エンチャントレベル3
+
 
     XRBaseController rightController;
+    XRBaseController leftController;
     Transform leftHand;
 
     public Transform rootPoint;
     public Transform tipPoint;
 
-    GameObject effect;
+    GameObject effect_level1;
+    GameObject effect_level2;
+    GameObject effect_level3;
 
     Vector3 prevLeftPos;
     Quaternion prevRot;
@@ -30,14 +40,22 @@ public class Sword_red : MonoBehaviour
 
     bool isActivated = false;
 
-    // 🔥 追加（振り状態管理）
+    //振り状態管理
     bool wasSwinging = false;
+
+    //エンチャントレベル
+    int enchantLevel = 0;
+
+    bool isAttack = false;
 
     void Start()
     {
         rightController = GameObject.Find("Right Controller").GetComponent<XRBaseController>();
+        leftController = GameObject.Find("Left Controller").GetComponent<XRBaseController>();
         leftHand = GameObject.Find("Left Controller").transform;
-        effect = transform.Find("Effect_Lightning").gameObject;
+        effect_level1 = transform.Find("Effect_Level1").gameObject;
+        effect_level2 = transform.Find("Effect_Level2").gameObject;
+        effect_level3 = transform.Find("Effect_Level3").gameObject;
 
         prevLeftPos = leftHand.position;
         prevRot = transform.rotation;
@@ -62,7 +80,7 @@ public class Sword_red : MonoBehaviour
             // ===== 振り判定 =====
             bool isSwinging = CheckSwing();
 
-            // 🔥 振り終わりで発動
+            // 振り終わりで発動
             if (!isSwinging && wasSwinging)
             {
                 ExecuteSkill();
@@ -72,7 +90,9 @@ public class Sword_red : MonoBehaviour
         }
         else
         {
-            effect.SetActive(false);
+            effect_level1.SetActive(false);
+            effect_level2.SetActive(false);
+            effect_level3.SetActive(false);
         }
 
         UpdatePreviousState();
@@ -107,6 +127,27 @@ public class Sword_red : MonoBehaviour
     // ===== エンチャント開始 =====
     void ActivateEnchant()
     {
+        // エンチャントレベルを上げる
+        if(enchantLevel <= 3)
+        {
+            enchantLevel++;
+        }
+        // エンチャントレベルに応じた振動
+        if(enchantLevel == 1)
+        {
+            Debug.Log("エンチャントレベル: " + enchantLevel);
+            SendHaptic(0.5f, 1f, leftController);
+        }
+        else if(enchantLevel == 2)
+        {
+            Debug.Log("エンチャントレベル: " + enchantLevel);
+            SendHaptic(0.8f, 1f, leftController);
+        }
+        else if(enchantLevel == 3)
+        {
+            Debug.Log("エンチャントレベル: " + enchantLevel);
+            SendHaptic(1f, 1f, leftController);
+        }
         Debug.Log("なぞり成功！");
         isActivated = true;
         cooldown = 0.3f;
@@ -115,8 +156,29 @@ public class Sword_red : MonoBehaviour
     // ===== エンチャント中処理 =====
     void UpdateEnchant()
     {
-        effect.SetActive(true);
-        SendHaptic(0.2f, 0.05f);
+        if(enchantLevel == 1)
+        {
+            effect_level1.SetActive(true);
+            effect_level2.SetActive(false);
+            effect_level3.SetActive(false);
+            SendHaptic(0.2f, 0.05f, rightController);
+        }
+        else if(enchantLevel == 2)
+        {
+            effect_level1.SetActive(false);
+            effect_level2.SetActive(true);
+            effect_level3.SetActive(false);
+            SendHaptic(0.5f, 0.05f, rightController);
+        }
+        else if(enchantLevel == 3)
+        {
+            effect_level1.SetActive(false);
+            effect_level2.SetActive(false);
+            effect_level3.SetActive(true);
+            SendHaptic(1f, 0.05f, rightController);
+        }
+        // effect.SetActive(true);
+        // SendHaptic(0.2f, 0.05f, rightController);
     }
 
     // ===== 振り判定 =====
@@ -134,10 +196,25 @@ public class Sword_red : MonoBehaviour
     void ExecuteSkill()
     {
         Debug.Log("スキル発動！");
+        if(enchantLevel == 1)
+        {
+            Debug.Log("エンチャントレベル1のスキル発動");
+            // Instantiate(fireEffect, tipPoint.position, fireEffect.transform.rotation);
+            Quaternion randomRot = Random.rotation;
 
-        Instantiate(explosionEffect, tipPoint.position, Quaternion.identity);
-        Instantiate(multiSlashEffect, tipPoint.position, Quaternion.identity);
+            Instantiate(fireEffect, tipPoint.position, randomRot);
+        }
+        else if(enchantLevel == 2)
+        {
+            Debug.Log("エンチャントレベル2のスキル発動");
+        }
+        else if(enchantLevel == 3)
+        {
+            Debug.Log("エンチャントレベル3のスキル発動");
+        }
+        
         isActivated = false;
+        enchantLevel = 0; // エンチャントレベルリセット
     }
 
     // ===== 前フレーム更新 =====
@@ -149,11 +226,36 @@ public class Sword_red : MonoBehaviour
     }
 
     // ===== 振動 =====
-    void SendHaptic(float amplitude, float duration)
+    void SendHaptic(float amplitude, float duration, XRBaseController controller)
     {
-        if (rightController != null)
+        if (controller != null)
         {
-            rightController.SendHapticImpulse(amplitude, duration);
+            controller.SendHapticImpulse(amplitude, duration);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //===エフェクトが出ない不具合===
+        // 敵に当たったときの処理
+        if(other.gameObject.CompareTag("Enemy") && !isAttack)
+        {
+            // 接触点を取得
+            ContactPoint contact = other.contacts[0];
+            // 接触点にエフェクトを生成
+            Vector3 hitPos = contact.point;
+
+            Instantiate(hitEffect, hitPos, Quaternion.identity);
+            isActivated = false;
+            enchantLevel = 0;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            isAttack = false;
         }
     }
 }
